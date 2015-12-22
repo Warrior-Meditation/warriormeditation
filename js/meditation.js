738a3645649d150284b196a454384921392c8ed2
David Swaintek <dswaintek@gmail.com>
2015-12-22 10:01:08 -0800

    Add play and stop buttons and functionality to kick out total time

--- js/meditation.js
+++ js/meditation.js
@@ -2,9 +2,45 @@ SC.initialize({
   client_id: '9586854c0f0c9138735b653b994f0fb5'
 });
 
-var track_url = 'https://soundcloud.com/david-swaintek/dragons-versus-robots';
+var currentTrack = SC.get('/tracks/238687124');
 
-SC.oEmbed(track_url, { auto_play: false, element: document.getElementById('player') })
-.then(function(oEmbed) {
-  console.log('oEmbed response: ', oEmbed);
+SC.stream('/tracks/238687124').then(function(player) {
+  $('#start').on('click', function () {
+    console.log('start');
+    console.log(player);
+    player.play();
+  });
+
+  $('#stop').on('click', function () {
+    console.log('stop');
+    timeCounter = player.currentTime();
+    console.log(timeCounter);
+    player.pause();
+  });
+
+  player.on('finish', function () {
+    timeCounter = currentTrack._result.duration;
+    console.log(timeCounter);
+    console.log('finish');
+  });
 });
+
+//   // SC.stream(currentTrack, function(sound){
+//     $('#start').click(function(e) {
+//       e.preventDefault();
+//       console.log('start');
+//       sound.start();
+//     });
+//     $('#stop').click(function(e) {
+//       e.preventDefault();
+//       console.log('stop');
+//       sound.stop();
+//     });
+//   });
+// // });
+
+
+
+// /tracks/181282422
+
+// client_id: '9586854c0f0c9138735b653b994f0fb5'

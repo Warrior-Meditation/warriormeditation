5fee8a241b5537ad3d9da7f361cf3c024dc1c69b
David Tolmie <dave@bodhitech.org>
2015-12-21 17:16:40 -0800

    Created initial user object based on methods from Firebase test scripts.

---
+++ js/user.js
@@ -0,0 +1,50 @@
+function User(props) {
+  this.fullName = props.fullName;
+  this.emailAddress = props.emailAddress;
+  this.timeMeditated = props.timeMeditated;
+  this.meditationData = props.meditationData;
+  this.lastMeditated = props.lastMeditated;
+  this.consecDays = props.consecDays;
+};
+
+User.authenticate = function (userEmail, userPassword) {
+  ref.authWithPassword({
+    email    : userEmail,
+    password : userPassword
+  }, function(error, authData) {
+    if (error) {
+      console.log('Login Failed!', error);
+    } else {
+      console.log('Authenticated successfully with payload:', authData);
+    }
+  });
+};
+
+User.authUser = function (event) {
+  event.preventDefault();
+  var userEmail = $('#loginEmail').val();
+  var userPassword = $('#loginPassword').val();
+  User.authenticate(userEmail, userPassword);
+};
+
+User.createUser = function (event) {
+  event.preventDefault();
+  var fullName = $('#formName').val();
+  var userEmail = $('#formEmail').val();
+  var userPassword = $('#formPassword').val();
+  ref.createUser({
+    email    : userEmail,
+    password : userPassword
+  }, function(error, userData) {
+    if (error) {
+      console.log('Error creating user:', error);
+    } else {
+      console.log('Successfully created user account with uid:', userData.uid);
+      ref.child('users').child(userData.uid).set({
+        name: fullName,
+        email: userEmail
+      });
+      User.authenticate(userEmail, userPassword);
+    }
+  });
+};

c7c7100304f10a651865b1fd01a9e421209db1a2
ERIKAWHO <Erika@RefreshMediaResources.com>
2015-12-28 16:28:16 -0800

    Hide and show user stats.

--- js/indexView.js
+++ js/indexView.js
@@ -6,5 +6,14 @@ indexView.init = function () {
     .siblings().hide();
   if (User.uid) {
     $('#user-scoring').show();
-  } 
+  }
+  indexView.displayStats();
+};
+
+indexView.displayStats = function() {
+  $('#stats').on('click', function() {
+    console.log($(this))
+    $('.popup').show();
+    // .attr('display', 'flex');
+  });
 };
